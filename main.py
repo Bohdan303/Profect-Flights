fd6f74697342972b0a2529d2b1039ee2754bd8aa
ghaz123-byte <ghazali.noorani@gmail.com>
2025-03-23 17:03:48 +0100

    removed zip elements were unnecessary

--- main.py
+++ main.py
@@ -6,7 +6,6 @@ from data_processing.orchestration import preprocess_data
 from visualizations.index import create_all_visualizations
 from dashboard.app import run_dashboard
 import pandas as pd
-import zipfile
 
 def load_preprocessed_data(preprocessed_db):
     conn = sqlite3.connect(preprocessed_db)
@@ -17,31 +16,10 @@ def load_preprocessed_data(preprocessed_db):
     df_airlines = pd.read_sql_query("SELECT * FROM airlines", conn)
     return df_airports, df_flights, df_planes, df_weather, df_airlines
 
-def unzip_preprocessed_db(zip_path="preprocessed_flights.zip", extract_to="./"):
-    """
-    Unzips the preprocessed database file if not already extracted.
-    """
-    db_path = os.path.join(extract_to, "preprocessed_flights.db")
     
-    if not os.path.exists(db_path):
-        if os.path.exists(zip_path):
-            with zipfile.ZipFile(zip_path, 'r') as zip_ref:
-                zip_ref.extractall(extract_to)
-            print(f"Preprocessed DB extracted to {extract_to}")
-        else:
-            print("No preprocessed DB zip found.")
-    else:
-        print("Preprocessed DB already extracted.")
-    
-    return db_path
-
-
 if __name__ == "__main__":
     conn, df_airports, df_flights, df_planes, df_weather, df_airlines = load_data()
     preprocessed_db = "preprocessed_flights.db"
-    preprocessed_zip = "preprocessed_flights.zip"
-    preprocessed_db = "preprocessed_flights.db"
-    unzip_preprocessed_db(preprocessed_zip)
     
     if os.path.exists(preprocessed_db):
         st.write("Loading preprocessed data from", preprocessed_db)
