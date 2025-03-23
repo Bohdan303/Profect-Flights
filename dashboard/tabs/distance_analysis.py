520f570f6474c72246fa29c2b28cc626e3f2cddd
ghaz123-byte <ghazali.noorani@gmail.com>
2025-03-23 23:08:50 +0100

    converted more stuff to sql

--- dashboard/tabs/distance_analysis.py
+++
@@ -1,8 +0,0 @@
-import streamlit as st
-
-def render(df_flights, visuals):
-    st.header("Distance Analysis")
-    st.subheader("Geodesic Distance Histogram")
-    st.plotly_chart(visuals.get("fig_distance_hist"))
-    st.subheader("Delay vs. Distance")
-    st.plotly_chart(visuals.get("fig_delay_vs_distance"))
--- dashboard/tabs/general_maps.py
+++
@@ -1,8 +0,0 @@
-import streamlit as st
-
-def render(df_airports, visuals):
-    st.header("General Maps")
-    st.subheader("World Airport Map")
-    st.plotly_chart(visuals.get("fig_alt"))
-    st.subheader("US Airport Map")
-    st.plotly_chart(visuals.get("fig_us"))
