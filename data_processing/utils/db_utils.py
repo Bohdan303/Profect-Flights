90eab21031c3930e2dbadccf08841598733d81b0
Bohdan303 <bohdan.kulczakowicz@gmail.com>
2025-03-23 14:06:15 +0100

    Update db_utils.py

--- data_processing/utils/db_utils.py
+++ data_processing/utils/db_utils.py
@@ -2,7 +2,7 @@ import os
 import sqlite3
 import pandas as pd
    
-def load_data(db_path="flights_database.db"):
+def load_data(db_path="flights_database.zip"):
     conn = sqlite3.connect(db_path)
     conn.execute("PRAGMA journal_mode = MEMORY")
     conn.execute("PRAGMA synchronous = OFF")
